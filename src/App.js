413be7d5f1ffc7da0bf15f7cae6ef96b3e2fd121
Blake M <Blake.manookin@gmail.com>
2019-04-22 20:20:08 -0600

    Creating some components

--- src/App.js
+++ src/App.js
@@ -6,12 +6,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 class App extends Component {
   render() {
     return (
-     <div className='container'>
-     <div className='row'>
-       <div className='col-6'>Column no 1</div>
-       <div className='col-6'><span><i className="fas fa-home"/> </span></div>
-     </div>
-     </div>
+        <React.Fragment>
+            <h3>Hello world</h3>
+        </React.Fragment>
     );
   }
 }
