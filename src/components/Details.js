a73cb8120506c8b975efe88ff9bcf9c5b4f88438
Blake M <Blake.manookin@gmail.com>
2019-04-28 13:34:38 -0600

    basic details page using template

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,7 +8,40 @@ class Details extends Component {
         return (
             <ProductConsumer>
                 {(value)=>{
+                    const {id,author,img,info,price,title,inCart} = value.detailProduct;
+                    return (
+                        <div className="container py-5">
 
+                            <div className="row">
+                                <div className="col-10 mx-auto text-center my-5 mainText">
+                                    <h1>{title}</h1>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    <img src={img} alt={title} className="img-fluid"/>
+                                </div>
+                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                    <h2 className='text-center'>{title}</h2>
+                                    <h4 className='text-center text-uppercase text-muted mt-3 mb-2'>{author}</h4>
+                                    <h4 className="mainText"><strong>Price: <span>${price}</span></strong></h4>
+                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                        comic description:</p>
+                                    <p className="text-muted lead">
+                                    {info}
+                                </p>
+                                    <div>
+                                        <Link to='/'>
+                                            <ButtonContainer>All Comics</ButtonContainer>
+                                        </Link>
+                                        <ButtonContainer disabled={!!inCart}>
+                                            {inCart ? 'In Cart': 'add to cart'}
+                                        </ButtonContainer>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
                 }}
             </ProductConsumer>
         );
