4f4df60bc319e004df537e3c8969abf97bcd435c
Blake M <Blake.manookin@gmail.com>
2019-04-29 00:21:29 -0600

    Updates

---
+++ precache-manifest.1ca8548f43a4466350b74f08be1c13fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a7a2372be74b7f6aa5",
+    "url": "/Blakem2399/e-commerse/static/css/main.d3beffaf.chunk.css"
+  },
+  {
+    "revision": "a0a7a2372be74b7f6aa5",
+    "url": "/Blakem2399/e-commerse/static/js/main.ca3ad019.chunk.js"
+  },
+  {
+    "revision": "d0a0506c0ddafcda9675",
+    "url": "/Blakem2399/e-commerse/static/js/runtime~main.74257ff0.js"
+  },
+  {
+    "revision": "cdf4b384cdda7145fd95",
+    "url": "/Blakem2399/e-commerse/static/css/2.d997a375.chunk.css"
+  },
+  {
+    "revision": "cdf4b384cdda7145fd95",
+    "url": "/Blakem2399/e-commerse/static/js/2.a29cd647.chunk.js"
+  },
+  {
+    "revision": "5886b146b2198aae29df614aba37f9be",
+    "url": "/Blakem2399/e-commerse/static/media/logo.5886b146.svg"
+  },
+  {
+    "revision": "3170d3f74f1b145ca53d1bd34294ebf4",
+    "url": "/Blakem2399/e-commerse/index.html"
+  }
+];
